2d56dd9668e612247df39117c8abe54f611ffc1b
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-08-06 22:52:04 +0800

    Extract char change

--- backend/blueprints/tools.py
+++ backend/blueprints/tools.py
@@ -12,7 +12,7 @@ def extract_chinese_between_chars(text, start, end=''):
     Returns:
         list: A list of strings containing the extracted Chinese characters.
     """
-    pattern = rf"{re.escape(start)}([\u3000-\u303F\u4E00-\u9FFF\uFF00-\uFFEF]+){re.escape(end)}"
+    pattern = rf"{re.escape(start)}\s*([\u3000-\u303F\u4E00-\u9FFF\uFF00-\uFFEF]+)\s*{re.escape(end)}"
     matches = re.findall(pattern, text, re.UNICODE)
     for i in range(len(matches)):
         matches[i] = matches[i][3:] if matches[i][:3] == start else matches[i]
