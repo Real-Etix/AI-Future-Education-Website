b9b9c73f0f7fad44c7605076edbf2dd460e6955e
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-08-09 17:42:56 +0800

    Allow streaming on llm completion

--- backend/blueprints/llm_call/llm_wrapper.py
+++ backend/blueprints/llm_call/llm_wrapper.py
@@ -13,16 +13,31 @@ client = openai.OpenAI(
 )
 bot = 'GPT-4o-mini'
 
-async def llm_response(prompt) -> str:
+async def llm_response(prompt, stream=False):
     '''
     Send message to and receive response from Poe chatbot. 
     '''
-    messages = [{'role': 'user', 'content': prompt}]
+    if isinstance(prompt, list):
+        # If prompt is a list, it is a message list
+        messages = prompt
+    else:
+        # If prompt is a string, it is a single message
+        messages = [{'role': 'user', 'content': prompt}]
     response = client.chat.completions.create(
         model="GPT-4o-mini", # or other models (Claude-Sonnet-4, Gemini-2.5-Pro, Llama-3.1-405B, Grok-4..)
         messages = messages, # type: ignore
+        stream=stream
     )
-    return response.choices[0].message.content if response.choices[0].message.content else ''
+    if stream:
+        for chunk in response:
+            yield chunk.choices[0].delta.content if chunk.choices[0].delta.content else ''
+    else:
+        yield response.choices[0].message.content if response.choices[0].message.content else ''
+
+async def llm_response_stream(prompt):
+        async for text in llm_response(prompt, stream=True):
+            print(text, end='', flush=True)
+        print()  # Ensure a newline at the end
 
 
 
@@ -34,17 +49,6 @@ if __name__ == '__main__':
     time2 = 0.0
     test_random_message = ['Test', "你好嗎？", "你在做什麼?", "嗯？", "Who are you?"]
     for i in range(5):
-        test = [{
-            'role': 'system', 'content': '你是一名測試員。',
-            'role': 'user', 'content': '''假設你是想學習承諾的使用者，問一個和承諾有關的問題測試意圖分析員。
-
-例子： 
-使用者： 我想知道什麼是諾言
-
-使用者：
-            '''
-        }]
-        test_message1 = asyncio.run(llm_response(test))
         prompt = f'''\
 你要從回答中分析使用者的意圖。意圖可能是
 - 承諾
@@ -56,13 +60,14 @@ if __name__ == '__main__':
 意圖： 承諾
 使用者： 你是誰？
 意圖： 問候
+"""
 
-{test_message1}
+使用者：{test_random_message[i]}
 意圖：'''
         messages1 = [{'role': 'user', 'content': prompt}]
+        print('1:', test_random_message[i], ':')
         start_time = time.time()
-        result = asyncio.run(llm_response(messages1))
-        print('1:', test_message1, '->', result)
+        asyncio.run(llm_response_stream(prompt))
         time1 += time.time() - start_time
         print(time.time() - start_time)
         messages2 = [{
@@ -79,55 +84,13 @@ if __name__ == '__main__':
 使用者： 你是誰？
 意圖： 問候
 """
-{test_message1}
-意圖：\
-'''
-        }]
-        start_time = time.time()
-        result = asyncio.run(llm_response(messages2))
-        print('2:', test_message1, '->', result)
-        time2 += time.time() - start_time
-        print(time.time() - start_time)
-        prompt = f'''\
-你要從回答中分析使用者的意圖。意圖可能是
-- 承諾
-- 問候
-- 沒有
 
-例子：
-使用者： 我想知道什麼是諾言。
-意圖： 承諾
-使用者： 你是誰？
-意圖： 問候
-"""
-{test_random_message[i]}
-意圖：'''
-        messages1 = [{'role': 'user', 'content': prompt}]
-        start_time = time.time()
-        result = asyncio.run(llm_response(messages1))
-        print('1:', test_random_message[i], '->', result)
-        time1 += time.time() - start_time
-        print(time.time() - start_time)
-        messages2 = [{
-            'role': 'system', 'content': '你是一名意圖分析員。',
-            'role': 'user', 'content': f'''\
-你要從回答中分析使用者的意圖。意圖可能是
-- 承諾
-- 問候
-- 沒有
-
-例子：
-使用者： 我想知道什麼是諾言。
-意圖： 承諾
-使用者： 你是誰？
-意圖： 問候
-"""
-{test_random_message[i]}
+使用者：{test_random_message[i]}
 意圖：'''
         }]
+        print('2:', test_random_message[i], ':')
         start_time = time.time()
-        result = asyncio.run(llm_response(messages2))
-        print('2:', test_random_message[i], '->', result)
+        asyncio.run(llm_response_stream(messages2))
         time2 += time.time() - start_time
         print(time.time() - start_time)
     print('Prompt 1 time:', time1)
--- backend/blueprints/llm_call/local_llm_wrapper.py
+++ backend/blueprints/llm_call/local_llm_wrapper.py
@@ -1,6 +1,7 @@
 # backend/blueprints/llm_call/local_llm_wrapper.py
 
 from llama_cpp import Llama
+from llama_cpp.llama_speculative import LlamaPromptLookupDecoding
 from huggingface_hub import hf_hub_download
 import multiprocessing
 # import opencc
@@ -10,31 +11,48 @@ import asyncio
 # endConverter = opencc.OpenCC('s2hk.json')
 
 class LocalLLM():
-    def __init__(self, model_path):
+    def __init__(self, model_path, n_ctx=1024, draft_model=None):
         self.model_path = model_path
+        self.draft_model = draft_model
+        self.n_ctx = n_ctx
+        print('Initialize LLM with init_llm() method.')
 
-    def init_llm(self):
+    def init_llm(self, verbose=False):
+        print('Initializing LLM...')
         self.llm = Llama(
             model_path=self.model_path,
-            n_ctx = 512,
+            n_ctx = self.n_ctx,
             n_threads = multiprocessing.cpu_count(),
-            verbose = False
+            draft_model = self.draft_model,
+            verbose = verbose
         )
+        print('Completed LLM initialization.')
 
-    async def local_llm_response(self, prompt, temperature=0, max_tokens=1024):
+    async def local_llm_completion(self, prompt, temperature=0.0, max_tokens=1024, stream=False):
         '''
-        Send message to and receive response from LLM locally. 
+        Send message to and receive response from LLM locally upon completion. 
         '''
         generation_kwargs = {
             "max_tokens": max_tokens,
             "stop": ['<|eot_id|>','<|end_of_text|>'],
             'temperature': temperature,
+            'stream': stream
         }
-        # prompt = startConverter.convert(prompt)
-        response = self.llm.create_completion(prompt, **generation_kwargs)
-        response_text = response['choices'][0]['text'] # type: ignore
-        # translated_text = endConverter.convert(response_text)
-        return response_text
+        if not self.llm:
+            yield "沒有"
+        if stream:
+            for chunk in self.llm.create_completion(prompt, **generation_kwargs):
+                text = chunk['choices'][0]['text'] # type: ignore
+                yield text
+        else:
+            response = self.llm.create_completion(prompt, **generation_kwargs)
+            response_text = response['choices'][0]['text'] # type: ignore
+            yield response_text 
+
+    async def local_llm_response_stream(self, prompt):
+        async for text in self.local_llm_completion(prompt, max_tokens=250, stream=True, temperature=0.8):
+            print(text, end='', flush=True)
+        print()  # Ensure a newline at the end
 
 
 
@@ -43,30 +61,62 @@ class LocalLLM():
 # This is for testing purposes to see if the prompt work properly.
 if __name__ == '__main__':
     model_name = "hfl/Llama-3-Chinese-8B-Instruct-v3-GGUF"
-    model_file = "ggml-model-q6_k.gguf"
-    print(1)
-    model_path = hf_hub_download(model_name, filename=model_file, local_dir='backend/model')
-    print(2)
-    llm = LocalLLM(model_path)
-    print(3)
-    llm.init_llm()
-    print(4)
+    # model_name = "shenzhi-wang/Llama3-8B-Chinese-Chat-GGUF-8bit"
+    filename = "ggml-model-q6_k.gguf"
+    # filename = "Llama3-8B-Chinese-Chat-q8_0-v2_1.gguf" # Performance worse and slower
+    model_path = hf_hub_download(model_name, filename=filename, local_dir='backend/model')
+    llm1 = LocalLLM(model_path)
+    llm1.init_llm()
+    llm2 = LocalLLM(model_path)
+    llm2.init_llm()
     print('Number of cores:', multiprocessing.cpu_count())
     test_random_message = ['承諾', "諾言", "怎樣遵守說過的？", "嗯？", "Who are you?"]
-    for i in range(5):
-        prompt1 = f'''<|start_header_id|>user<|end_header_id|>
+    test_story = [''.join(["春秋時期，吳國貴族季札出使拜訪徐國。徐國國君在接待季札時，看到了他佩帶",
+        "的寶劍，流露出喜愛之情。細心的季札看出徐國國君的心意，但作為吳國使節，他到",
+        "各諸侯國拜訪時不能沒有寶劍作配飾，這是一種外交禮儀，他不能不遵守，有辱自己",
+        "的使命。於是季札向他承諾，完成出使後，必定把寶劍送給徐國國君。\n",
+        "過了一段時間，季札終於完成出使任務，回程時途經徐國，他想去拜訪徐國國君，",
+        "以贈送寶劍，卻驚訝發現徐國國君已死。季札感到十分遺憾，萬分悲痛地來到徐國國",
+        "君墓前祭奠。祭奠完畢，他解下身上的寶劍，把它掛在墓旁的樹上。這時，侍從疑惑",
+        "地問，為甚麼徐國國君已死，季札仍要留下珍貴的寶劍呢？季札解釋，當時他已承諾",
+        "給徐國國君贈劍，不能因為徐國國君已死，就違背自己的諾言。"]),
+        ''.join(["曾子是孔子的學生，為人很有信用，說到做到。\n",
+        "一天，曾子的妻子要到市集，兒子哭哭鬧鬧地嚷着要跟媽媽一起",
+        "上街。曾子的妻子不想帶着兒子去，便哄他說：「乖孩子，你先回家",
+        "吧！待我回來，馬上宰了那頭胖胖白白的豬，給你弄一頓好吃的。」",
+        "孩子聽到有豬肉吃，就不再哭鬧了。\n",
+        "過了不久，妻子從市集回來，看見曾子拿起刀來，果真準備手起",
+        "刀落，宰了那頭豬。妻子馬上三步拼作兩步，衝上前去阻止曾子說：",
+        "「我剛才不過是跟孩子開個玩笑罷了，不是真的要把豬殺掉！」這時",
+        "曾子一臉嚴肅地說：「我們不能跟兒子開這樣的玩笑！小孩子不懂",
+        "事，只會聽從父母的教導，模仿父母的言行。為人父母者，必須以身",
+        "作則，答應孩子的事，就要想辦法做到。今天如果我們不殺豬，就是",
+        "欺騙兒子，也就等如教他欺騙別人。做母親的欺騙兒子，兒子以後都",
+        "不會再相信母親了。這種事絕不能開玩笑，更不是教育孩子的方法！」\n",
+        "妻子聽了點點頭。曾子於是把豬殺了，烹煮給兒子吃。"])]
+    for i in range(2):
+#         prompt1 = f'''<|start_header_id|>user<|end_header_id|>
         
-從回答分類用戶的意圖，意圖一定是：承諾、問候、沒有
-如果不清楚，回答"沒有"。
+# 分類用戶的意圖，意圖一定是：承諾、問候、沒有
+# 如果不清楚，回答"沒有"。
+
+# 用戶：{test_random_message[i]}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+# 意圖：'''
+#故事要二百字內
+        prompt2 = f'''<|start_header_id|>user<|end_header_id|>
+        
+創造二百字內的短故事，關於承諾，不用說故事告訴我們什麼。
 
 例子："""
-用戶：我想知道什麼是諾言。
-意圖：承諾
-用戶：你是誰？
-意圖：問候
-"""
+故事：{test_story[i]}
+"""<|eot_id|><|start_header_id|>assistant<|end_header_id|>
 
-用戶：{test_random_message[i]}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+故事：'''
+        asyncio.run(llm1.local_llm_response_stream(prompt2))
+        # text = asyncio.run(collect_response(response))
+        # print(test_random_message[i], '->', response)
+        # response = asyncio.run(llm2.local_llm_completion(prompt2, temperature=0.8, max_tokens = 100))
+        # text = asyncio.run(collect_response(response))
+        # print(test_random_message[i], '->', response)
 
-意圖：'''
-        print(test_random_message[i], '->', asyncio.run(llm.local_llm_response(prompt1, max_tokens=5)))
