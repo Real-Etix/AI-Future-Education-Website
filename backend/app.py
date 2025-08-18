ef54274ed3d33cc915468748c2f44e400f7b4837
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-08-19 01:47:07 +0800

    Fixing refreshing problem

--- backend/app.py
+++ backend/app.py
@@ -8,23 +8,20 @@ from flask_app import init_app
 
 app = init_app()
 
-# Render index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/AI-Future-Education-Website/', methods=['GET'])
-def main():
-    return render_template('index.html')
-
-@app.route('/AI-Future-Education-Website/<path:path>', methods=['GET'])
-def subsequent(path):
-    return render_template('index.html')
-
 # Render static files
 @app.route('/AI-Future-Education-Website/static/<path:path>', methods=['GET'])
 def serve_static(path):
-    return send_from_directory(app.static_folder, path) # type: ignore
+    return app.send_static_file(path)
+
+# Render index.html
+@app.route('/', defaults={'path': ''})
+@app.route('/AI-Future-Education-Website/<path:path>', methods=['GET'])
+def catch_all(path):
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False)
+    app.run(debug=False)
