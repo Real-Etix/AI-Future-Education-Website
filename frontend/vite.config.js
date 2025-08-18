e769bce875e7ddf5bd47a179f503c0fbd5d17a74
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-08-19 01:46:30 +0800

    Add chunking files to see file size of each frontend import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,23 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-// import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/AI-Future-Education-Website',
-  // base: '/',
   build: { 
     cssCodeSplit: true,
-    minify: false, // Added to stop compacting
-    // target: 'modules',
     rollupOptions: {
-      // external: ['vue'],
-      // input: {
-        // main: resolve(__dirname, 'index.html'),
-        // sidebar: resolve(__dirname, 'src/components/sidebar.html'),
-        // home: resolve(__dirname, 'src/views/home/index.html')
-      // },
       output: {
         dir: 'dist/',
         entryFileNames: 'static/js/[name]-entry.js',
@@ -35,10 +25,12 @@ export default defineConfig({
           return 'static/[name]-[hash][extname]';
         },
         chunkFileNames: 'static/js/[name]-chunk.js',
-        manualChunks: undefined,
-        // globals: {
-          // vue: 'Vue'
-        // }
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            // Group node_modules into a 'vendor' chunk
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
       },
     },
   },
